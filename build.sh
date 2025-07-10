ae7bf2505fcb5f44f24a18691c011f5d41119617
BONTHAPALLI ADITYA RAO <bonthapalliadityarao@gmail.com>
2025-07-10 12:49:29 +0530

    updated deployment settings

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
+#!/usr/bin/env bash
 set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input 
+cd backend  # 👈 Go into the folder containing manage.py
 
+python manage.py collectstatic --no-input
 python manage.py migrate
-
